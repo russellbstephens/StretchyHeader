c42f0941d98dbceea59b3fcd17c8f0e910e31a10
Russell Stephens <rstephens@tonicdesign.com>
2016-02-28 14:43:31 -0500

    Initial Commit

---
+++ StretchyHeader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StretchyHeader
+//
+//  Created by Russell Stephens on 2/28/16.
+//  Copyright © 2016 Russell Stephens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
