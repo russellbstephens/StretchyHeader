2e50dc44831cd3b1b82fd613318c15f02fe0ecb9
Russell Stephens <rstephens@tonicdesign.com>
2016-02-28 15:13:34 -0500

    adding header view

---
+++ StretchyHeader/ImageHeader.swift
@@ -0,0 +1,9 @@
+//  Copyright © 2016 Russell Stephens. All rights reserved.
+//
+
+import UIKit
+
+class ImageHeader: UICollectionReusableView {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
